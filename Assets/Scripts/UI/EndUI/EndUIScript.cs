97277009228b966b9f658944e3c7691ed6a6a4bd
Yaman AbdulFatah <58285821+yaman3bd@users.noreply.github.com>
2021-02-06 15:12:08 +0300

    EndUI

---
+++ Assets/Scripts/UI/EndUI/EndUIScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+public class EndUIScript : GlobalUIScript
+{
+    [Header("Images")]
+    public Image Background;
+
+    [Header("Texts Rects")]
+    public RectTransform EndStateTextLeftRect;
+    public RectTransform EndStateTextRightRect;
+    public RectTransform EndStateTextOverlayRect;
+    [Header("Texts Targets")]
+    public RectTransform EndStateTextLeftStartTargetRect;
+    public RectTransform EndStateTextRightStartTargetRect;
+    public RectTransform TextsEndTargetRect;
+    [Header("Texts Elements")]
+    public TMP_Text EndStateTextLeft;
+    public TMP_Text EndStateTextRight;
+    public TMP_Text EndStateTextOverlay;
+
+
+    private void InitTexts(bool winner)
+    {
+        Background.SetAlpha(0);
+
+        float yPos = TextsEndTargetRect.anchoredPosition.y;
+
+        EndStateTextLeftStartTargetRect.anchoredPosition = new Vector2(EndStateTextLeftStartTargetRect.anchoredPosition.x, yPos);
+
+        EndStateTextLeftRect.anchoredPosition = EndStateTextLeftStartTargetRect.anchoredPosition;
+        EndStateTextLeft.SetAlpha(0);
+        EndStateTextRightStartTargetRect.anchoredPosition = new Vector2(EndStateTextRightStartTargetRect.anchoredPosition.x, yPos);
+
+        EndStateTextRightRect.anchoredPosition = EndStateTextRightStartTargetRect.anchoredPosition;
+        EndStateTextRight.SetAlpha(0);
+
+        EndStateTextOverlayRect.transform.localScale = new Vector2(3, 3);
+
+        EndStateTextOverlay.SetAlpha(0);
+
+        string winnerText = "Winner";
+
+        if (!winner)
+        {
+            winnerText = "Loser";
+        }
+
+        EndStateTextLeft.text = winnerText;
+
+        EndStateTextRight.text = winnerText;
+
+        EndStateTextOverlay.text = winnerText;
+    }
+
+    public float BackgroundFadeDuration;
+    public float LeftRightTextsMoveDuration;
+    public float LeftRightTextsFadeDuration;
+
+    public float OverlayTextMoveDuration;
+    public float OverlayTextScaleDuration;
+    public float OverlayTextFadeDuration;
+
+    private void Animation()
+    {
+        InitTexts(true);
+
+        Sequence seq = DOTween.Sequence();
+       
+        seq.AppendCallback(() =>
+        {
+            
+            Background.DOFade(0.8f, BackgroundFadeDuration);
+
+            EndStateTextLeftRect.DOAnchorPos(TextsEndTargetRect.anchoredPosition, LeftRightTextsMoveDuration);
+            EndStateTextLeft.DOFade(1, LeftRightTextsFadeDuration);
+
+        }).AppendCallback(() =>
+        {
+        
+            EndStateTextRightRect.DOAnchorPos(TextsEndTargetRect.anchoredPosition, LeftRightTextsMoveDuration);
+            EndStateTextRight.DOFade(1, LeftRightTextsFadeDuration);
+
+        }).AppendCallback(() =>
+        {
+
+            EndStateTextOverlayRect.DOAnchorPos(TextsEndTargetRect.anchoredPosition, OverlayTextMoveDuration);
+            EndStateTextOverlayRect.DOScale(1, OverlayTextScaleDuration);
+            EndStateTextOverlay.DOFade(1, OverlayTextFadeDuration);
+
+        });
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Animation();
+        }
+    }
+
+}
